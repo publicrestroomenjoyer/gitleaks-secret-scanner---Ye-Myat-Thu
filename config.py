4094017b4cc566b3286496defbfc1e1784543fb2
publicrestroomenjoyer <richardloda07@gmail.com>
2026-06-25 17:58:56 +0900

    Add fake AWS access key for testing
    Added a fake AWS access key for classroom testing purposes.

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-# Safe version: no hardcoded secret is stored in the source code.
-# The value should come from an environment variable or GitHub Secret.
-
 import os
 
-AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+# Fake secret for classroom testing only.
+# Do not use real credentials.
+
+AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7EXAMPLE"
